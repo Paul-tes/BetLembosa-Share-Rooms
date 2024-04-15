8a1db46a5a49f767ed57457731330af105cd7f00
paul-tes <paulasca8462@gmail.com>
2024-04-15 14:25:30 +0300

    Add Assets and them configuration on telwindcss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export const nextConfig = {
+  reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dctahvizk",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const layout = () => {
+  return <html>
+    <body>
+    <div>layout</div>
+    </body>
+    </html>
+};
+
+export default layout;
