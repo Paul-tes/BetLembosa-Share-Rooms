72eaa8afdfc300078f87d63b02ec4f77635e8fe7
paul-tes <paulasca8462@gmail.com>
2024-04-18 13:56:52 +0300

    Add footter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 import React from "react";
 
 const page = () => {
   return <div>
     <Navbar/>
+    <Footer />
   </div>;
 };
 
