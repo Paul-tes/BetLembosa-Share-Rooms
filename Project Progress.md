9ac07573f0d8f5e881877af70456bcb36aec9a4d
paul-tes <paulasca8462@gmail.com>
2024-04-19 17:56:38 +0300

    Add Menue options list

---
+++ Project Progress.md
@@ -0,0 +1,52 @@
+# Timestamps
+
+- [x] 00:00:00 Demo
+- [x] 00:07:43 Amplication Intro
+- [x] 00:08:48 Amplication Project Setup
+- [x] 00:10:33 Airbnb Amplication Entities
+- [x] 00:19:10 Project Setup
+- [x] 00:22:30 Amplication Discord and Github
+- [x] 00:23:00 Amplication Upload Prisma Schema Feature Intro
+- [x] 00:23:40 Airbnb Clone Project Architecture Intro
+- [x] 00:26:00 Project Initial Setup
+- [x] 00:27:00 Navbar
+- [x] 00:39:04 Footer
+- [ ] 00:46:50 Auth Modal
+- [ ] 01:28:21 Zustand Setup
+- [ ] 01:35:55 Signup & Login
+- [ ] 02:13:00 Create Listing
+- [ ] 04:45:55 View List View Listings
+- [ ] 05:08:00 View Map View Listings
+- [ ] 05:31:25 My Listings Page
+- [ ] 05:50:33 Wishlists Page
+- [ ] 05:55:30 Listing Page
+
+## 📚 Links and References
+
+- [Starter Code](https://github.com/koolkishan/nextjs)
+- [React Map GL](https://visgl.github.io/react-map-gl/)
+- [Cloudinary](https://cloudinary.com/)
+- [Blog Post for Amplication Setup](https://dev.to/koolkishan/full-stack-...)
+
+## Futures of the AirBnb Clone
+ 
+- Built using Next.js
+- Styled with Tailwind CSS
+- Backend generated using Amplication
+- State Management with Zustand
+- Realtime Maps using Mapbox API 
+- Login Signup using JWT Tokens
+- API Call's using Axios
+
+## Functionalites of the AirBnb Clone
+
+- Create Listings
+- View Listings
+- Book Listings
+- Delete Listingsof the AirBnb Clone
+- Add Listing to Wishlist
+- Remove Listing from Wishlist
+- View booked Listings
+- Search Listings
+- Search Listings using Map
+- View Listings Data on Map
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import BetLembosaLogo from "../../svg/betlemobsaLogo.png"
 import { FiGlobe } from "react-icons/fi"
 import { RxHamburgerMenu } from "react-icons/rx"
+import ContextMenu from "../common/ContextMenu";
 
 
 const Navbar = () => {
-  return <div>
+  // use state for top navBar unmberger menue
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false)
+
+  // list of options that are appear in menu
+  const contextMenuOptions = [
+    {
+      name: "LogIn",
+      callback: () => {
+        setIsContextMenuVisible(false);
+      }
+    },
+
+    {
+      name: "SignUp",
+      callback: () => {
+        setIsContextMenuVisible(false);
+      }
+    },
+
+    {
+      name: "Stay BetLembosa",
+      callback: () => {
+        setIsContextMenuVisible(false);
+      }
+    },
+
+    {
+      name: "Help",
+      callback: () => {
+        setIsContextMenuVisible(false);
+      }
+    }
+  ];
+
+  return (
     <header className="w-full flex flex-col justify-center transition-all duration-30 h-20 border-b border-b-gray-200">
       <div className="flex items-center justify-between px-20">
         <div className="flex-grow basis-0">
@@ -26,7 +63,10 @@ const Navbar = () => {
             <li className="cursor-pointer">
             <FiGlobe />
             </li>
-            <li className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-xl transition-all duration-500">
+            <li
+              className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-xl transition-all duration-500"
+              onClick={() => setIsContextMenuVisible(true)}
+            >
               <RxHamburgerMenu />
               <span>
                 <Image 
@@ -40,8 +80,19 @@ const Navbar = () => {
           </ul>          
         </div>
       </div>
+      {isContextMenuVisible && (
+        <ContextMenu
+          contextMenu={isContextMenuVisible}
+          setContextMenue={setIsContextMenuVisible}
+          cordinates={{
+            x: 250,
+            y: 70,
+          }}
+          optios={contextMenuOptions}
+        />
+      )}
     </header>
-  </div>;
+  );
 };
 
 export default Navbar;
